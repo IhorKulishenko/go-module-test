f281a47b17561efff804e6fe3ea01e591851e6cc
IhorK <ihor.kulishenko@gmail.com>
2025-05-06 17:29:39 +0300

    added godoc

--- add.go
+++ add.go
@@ -1,5 +1,9 @@
+// Package for adding two integer numbers
+
 package exercise
 
+// Adds two integer numbers and returns result
+// more on [link]: https://www.mathsisfun.com/numbers/addition.html
 func Add(a, b int) int {
 	return a + b
 }
